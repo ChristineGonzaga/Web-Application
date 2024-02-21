a092370399b6b178279f6f090f41039b9ec49c9c
ChristineGonzaga <145411647+ChristineGonzaga@users.noreply.github.com>
2024-02-21 18:54:59 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/index.css">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar">
+    <ul>
+        <li><a href="index.php">STUDENT LIST</a></li>
+        <li><a href="attendance.php">ATTENDANCE</a></li>
+        <li><a href="edit_profile.php">EDIT PROFILE</a></li>
+        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="logout.php">LOG OUT</a></li>
+        
+    </ul>
+    <form action="index.php" method="GET" class="mb-3">
+        <input type="text" name="search" placeholder="Search by name">
+        <button type="submit" class="btn btn-primary">Search</button>
+    </form>
+
+    <nav class="navbar1">
+        <div class="container-fluid">
+        </div>
+    </nav>
+</nav>
+<div class="section">
+            <div class="hero">
+                <article>
+                    <h1>About</h1>
+                    <content>
+                    Student Attendance Checker are aware of how critical accurate attendance records are to creating a positive learning atmosphere. Our platform is made to make taking attendance easier, giving administrators and teachers the ability to easily and effectively monitor student attendance. <br> <br> <br>
+                    Dave T. Montesino - Programmer <br>
+                    Christine B. Gonzaga - UI Designer <br>
+                </content>
+                </article>
+            </div>
+        </div>
---
+++ edit_profile.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['valid'])) {
+    header("Location: login.php");
+    exit();
+}
+
+include("conn.php");
+
+if (isset($_SESSION['id'])) {
+    $user_id = $_SESSION['id'];
+    $query = "SELECT * FROM users WHERE id = $user_id";
+
+    $result = mysqli_query($con, $query);
+
+    if (!$result) {
+        die("Query Error: " . mysqli_error($con));
+    }
+
+    $user = mysqli_fetch_assoc($result);
+} else {
+    die("User ID is not set in session.");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $new_username = mysqli_real_escape_string($con, $_POST['username']);
+    $new_email = mysqli_real_escape_string($con, $_POST['email']);
+    $new_password = mysqli_real_escape_string($con, $_POST['password']);
+
+    $update_query = "UPDATE users SET email = '$new_email', password = '$new_password' WHERE id = $user_id";
+
+    if (mysqli_query($con, $update_query)) {
+
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error updating record: " . mysqli_error($con);
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Edit Profile</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box">
+            <div class="content">
+                <h1>Edit Profile</h1>
+            </div>
+            <hr>
+            <br> 
+            <form action="" method="post">
+                <div class="field input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" value="<?php echo $user['username']; ?>" autocomplete="off" required>
+                </div>
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" value="<?php echo $user['email']; ?>" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" value="<?php echo $user['password']; ?>" autocomplete="off" required>
+                </div>
+
+                <div class="field">
+            <input type="submit" class="btn" name="submit" value="Update">
+            </div>
+            <div class="field">
+                <input type="button" class="btn" onclick="window.location.href='index.php';" value="Go back">
+            </div>
+
+            </form>
+        </div>
+    </div>
+</body>
+</html>
