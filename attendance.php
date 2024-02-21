148685ca8d5a1b61063606ed46f3ec5fe7abbced
ChristineGonzaga <145411647+ChristineGonzaga@users.noreply.github.com>
2024-02-21 18:56:34 +0800

    attendance.php

--- attendance.php
+++ attendance.php
@@ -15,16 +15,19 @@ if (!isset($_SESSION['valid'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="index.css">
     <title>Attendance System</title>
 </head>
 <body>
     <nav class="navbar">
-        <ul>
-            <li><a href="index.php">STUDENT LIST</a></li>
-            <li><a href="attendance.php">ATTENDANCE</a></li>
-            <li><a href="">LOG OUT</a></li>
-        </ul>
+       <ul>
+        <li><a href="index.php">STUDENT LIST</a></li>
+        <li><a href="attendance.php">ATTENDANCE</a></li>
+        <li><a href="edit_profile.php">EDIT PROFILE</a></li>
+        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="logout.php">LOG OUT</a></li>
+        
+    </ul>
         <nav class="navbar1">
             <div class="container-fluid">
             </div>
